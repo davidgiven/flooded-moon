9e6fc6ba52ac9693991e915554f3415481635bd9
dg <none@none>
2014-05-15 22:38:29 +0000

    More matrix stuff. Possibly accurate...

--- terrainmaker2/matrices.adb
+++ terrainmaker2/matrices.adb
@@ -16,6 +16,17 @@ package body Matrices is
 		return r;
 	end;
 
+	function "-"(left, right: Matrix) return Matrix is
+		r: Matrix;
+	begin
+		for y in 0..3 loop
+			for x in 0..3 loop
+				r(x, y) := left(x, y) - right(x, y);
+			end loop;
+		end loop;
+		return r;
+	end;
+
 	function "*"(left, right: Matrix) return Matrix is
 		r: Matrix;
 		sum: Number;
@@ -32,8 +43,66 @@ package body Matrices is
 		return r;
 	end;
 
+	function Transpose(m: Matrix) return Matrix is
+		r: Matrix;
+	begin
+		for x in 0..3 loop
+			for y in 0..3 loop
+				r(x, y) := m(y, x);
+			end loop;
+		end loop;
+		return r;
+	end;
+
+	function Invert(m: Matrix) return Matrix is
+		r: Matrix;
+		invdet, det: Number;
+
+		function minor(r0, r1, r2, c0, c1, c2: integer) return Number is
+		begin
+			return
+				m(r0, c0) * (m(r1, c1)*m(r2, c2) - m(r2, c1)*m(r1, c2)) -
+				m(r0, c1) * (m(r1, c0)*m(r2, c2) - m(r2, c0)*m(r1, c2)) +
+				m(r0, c2) * (m(r1, c0)*m(r2, c1) - m(r2, c0)*m(r1, c1));
+		end;
+	begin
+		r(0, 0) :=  minor(1, 2, 3, 1, 2, 3);
+		r(1, 0) := -minor(0, 2, 3, 1, 2, 3);
+		r(2, 0) :=  minor(0, 1, 3, 1, 2, 3);
+		r(3, 0) := -minor(0, 1, 2, 1, 2, 3);
+
+		r(0, 1) := -minor(1, 2, 3, 0, 2, 3);
+		r(1, 1) :=  minor(0, 2, 3, 0, 2, 3);
+		r(2, 1) := -minor(0, 1, 3, 0, 2, 3);
+		r(3, 1) :=  minor(0, 1, 2, 0, 2, 3);
+
+		r(0, 2) :=  minor(1, 2, 3, 0, 1, 3);
+		r(1, 2) := -minor(0, 2, 3, 0, 1, 3);
+		r(2, 2) :=  minor(0, 1, 3, 0, 1, 3);
+		r(3, 2) := -minor(0, 1, 2, 0, 1, 3);
+
+		r(0, 3) := -minor(1, 2, 3, 0, 1, 2);
+		r(1, 3) :=  minor(0, 2, 3, 0, 1, 2);
+		r(2, 3) := -minor(0, 1, 3, 0, 1, 2);
+		r(3, 3) :=  minor(0, 1, 2, 0, 1, 2);
+										 
+		det := m(0, 0)*minor(1, 2, 3, 1, 2, 3) -
+			   m(1, 0)*minor(1, 2, 3, 0, 2, 3) +
+			   m(2, 0)*minor(1, 2, 3, 0, 1, 3) -
+			   m(3, 0)*minor(1, 2, 3, 0, 1, 2);
+
+		invdet := 1.0 / det;
+		for x in 0..3 loop
+			for y in 0..3 loop
+				r(x, y) := r(x, y) * invdet;
+			end loop;
+		end loop;
+		
+		return r;
+	end;
+
 	function ToString(m: Matrix) return string is
-		LF: string := (1 => Ada.Characters.Latin_1.LF);
+		LF: character := ASCII.LF;
 
 		function S(x, y: integer) return String is
 			(Number'Image(m(x, y)));
--- terrainmaker2/matrices.ads
+++ terrainmaker2/matrices.ads
@@ -5,7 +5,10 @@ use Config;
 use Vectors;
 
 package Matrices is
+	-- Column-major, so the order is x, y!
 	type Matrix is array(integer range 0..3, integer range 0..3) of Number;
+	-- Ensure column-major order, not row-major order.
+	pragma Convention(Fortran, Matrix);
 
 	Identity: constant Matrix := (
 		(1.0, 0.0, 0.0, 0.0),
@@ -14,8 +17,18 @@ package Matrices is
 		(0.0, 0.0, 0.0, 1.0)
 	);
 
-	function "+"(left, right: Matrix) return Matrix;
-	function "*"(left, right: Matrix) return Matrix;
+	Zero: constant Matrix := (
+		(0.0, 0.0, 0.0, 0.0),
+		(0.0, 0.0, 0.0, 0.0),
+		(0.0, 0.0, 0.0, 0.0),
+		(0.0, 0.0, 0.0, 0.0)
+	);
+
+	function "+" (left, right: Matrix) return Matrix;
+	function "-" (left, right: Matrix) return Matrix;
+	function "*" (left, right: Matrix) return Matrix;
+
+	function transpose(m: Matrix) return Matrix;
 
 	function ToString(m: Matrix) return string;
 end;
